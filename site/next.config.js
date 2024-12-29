062109dc652468ed5bcdd1e3e4436fdc76faa4f4
Christopher Schnick <crschnick@xpipe.io>
2024-12-29 01:58:53 +0100

    Apply changes

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'XPipe Webtop',
+    description: 'https://github.com/xpipe-io/xpipe-webtop',
+    icon: 'https://xpipe.io/logo.svg',
+    listUrl: 'https://kasm.xpipe.io/',
+    contactUrl: 'https://xpipe.io',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
